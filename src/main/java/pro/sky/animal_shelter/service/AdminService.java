91f2a70255c560a66713405b414332dff6d9fec0
FeralHorse <pusiket90@yandex.ru>
2024-05-03 13:08:23 +0300

    Добавлен сервис для админ команд AdminService - Добавлен метод проверки является ли пользователь администратором - Добавлен метод, чтобы стать администратором. (для этого надо отправить токен бота в чат) Создан сервис для всплывающих кнопок под сообщениями - Вынесен код из основного класса TelegramBot - Разделены на методы создаваемые кнопки для администрации и пользователей.

---
+++ src/main/java/pro/sky/animal_shelter/service/AdminService.java
@@ -0,0 +1,42 @@
+package pro.sky.animal_shelter.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import pro.sky.animal_shelter.model.*;
+
+@Slf4j
+@Service
+public class AdminService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private AboutRepository aboutRepository;
+    @Autowired
+    private ContactInformationRepository contactInformationRepository;
+    @Autowired
+    private InfoRepository infoRepository;
+    @Autowired
+    private PetRepository petRepository;
+    public boolean checkAdmin(long chatId){
+        var user = userRepository.findById(chatId);
+        StringBuilder role = new StringBuilder();
+        if(user.isEmpty()) {
+            return false;
+        } else{
+            user.ifPresent(item->role.append(item.getRole()));
+        }
+        return role.toString().equals("admin");
+    }
+    public void setRole(Message message){
+        User user = new User();
+        user.setChatId(message.getChatId());
+        user.setFirstName(message.getChat().getFirstName());
+        user.setLastName(message.getChat().getLastName());
+        user.setUserName(message.getChat().getUserName());
+        user.setRole("admin");
+        userRepository.save(user);
+        log.info("user saved: " + user);
+    }
+}
